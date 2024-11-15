778f0e1bff219d7a903d98ea43e2e5264665f786
Marco Minerva <marco.minerva@gmail.com>
2024-11-15 16:55:24 +0100

    Add project files.

---
+++ ScribanSample/OrderConfirmation.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+using Scriban;
+using Scriban.Runtime;
+
+namespace ScribanSample;
+
+public static class OrderConfirmation
+{
+    public static async Task<string> CreateOrderAsync()
+    {
+        var template = Template.Parse("""
+        <html>
+            <head>
+                <style media="all" type="text/css">
+                html {
+                    font-size: 16px;
+                    font-family: Helvetica Neue, Helvetica, Arial, and sans-serif;
+                }
+                table {
+                        border-collapse: collapse;
+                        width: 100%;
+                    }
+                    th, td {
+                        font-size: 16px;
+                        padding: 8px;
+                        text-align: left;
+                    }
+                    th {
+                        background-color: green;
+                        color: white;
+                    }
+                    .right { 
+                        text-align: right; 
+                    }
+                    .even {
+                        background-color: #f2f2f2;
+                    }
+                    .odd {
+                        background-color: #ffffff;
+                    }
+                </style>
+            </head>
+            <body>
+                <p>
+                    Dear {{ Model.CustomerName }},
+                    <br />
+                    Here it is the details of the order you placed on {{ Model.Date | date.to_string '%d/%m/%Y %R' }}.
+                </p>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th class="right">Price</th>
+                            <th class="right">Quantity</th>
+                            <th class="right">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {{- for product in Model.Products }}
+                            <tr class="{{ if for.even }}even{{ else }}odd{{ end }}">
+                                <td>{{ product.Name }}</td>
+                                <td>{{ product.Description }}</td>
+                                <td class="right">{{ product.UnitPrice | object.format "C" }}</td>
+                                <td class="right">{{ product.Quantity }}</td>
+                                <td class="right">{{ product.TotalPrice | object.format "C" }}</td>
+                            </tr>
+                        {{- end }}
+                    </tbody>
+                </table>
+                <p align="right">Total: {{ Model.Total | object.format "C" }}</p>
+            </body>
+        </html>
+        """);
+
+        var productFaker = new Faker<Product>();
+        productFaker.RuleFor(p => p.Name, f => f.Commerce.ProductName());
+        productFaker.RuleFor(p => p.Description, f => f.Commerce.ProductDescription());
+        productFaker.RuleFor(p => p.UnitPrice, f => f.Random.Decimal(1, 100));
+        productFaker.RuleFor(p => p.Quantity, f => f.Random.Number(1, 10));
+
+        var order = new Order
+        {
+            CustomerName = "Marco",
+            Date = DateTime.Now,
+            Products = productFaker.Generate(7)
+        };
+
+        var context = new TemplateContext { MemberRenamer = member => member.Name };
+        context.PushGlobal(new ScriptObject { { "Model", order } });
+        context.PushCulture(CultureInfo.CurrentCulture);
+
+        var result = await template.RenderAsync(context);
+        return result;
+    }
+}
+
+public class Order
+{
+    public string? CustomerName { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public IList<Product> Products { get; set; } = [];
+
+    public decimal Total => Products?.Sum(p => p.TotalPrice) ?? 0;
+}
+
+public class Product
+{
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal TotalPrice => UnitPrice * Quantity;
+}
---
+++ ScribanSample/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Globalization;
+using Scriban;
+using Scriban.Runtime;
+using ScribanSample;
+
+var template = Template.Parse("Hello {{ name }}!");
+var result = await template.RenderAsync(new { Name = "World" });
+
+Console.WriteLine(result);
+Console.WriteLine();
+
+var people = new List<Person>
+{
+    new("Marco", "Minerva", "Taggia"),
+    new("Donald", "Duck", "Paperopoli"),
+    new("Scrooge", "McDuck", "Paperopoli"),
+};
+
+template = Template.Parse("""
+    <ul id="people">
+        {{- for person in Model.People }}
+        <li>
+            {{ person.FirstName }} {{ person.LastName }} from {{ person.City }}
+        </li>
+        {{- end }}
+    </ul>
+    """);
+
+var context = CreateContext(new PeopleModel { People = people });
+result = await template.RenderAsync(context);
+
+Console.WriteLine(result);
+Console.WriteLine();
+
+var products = new List<Product>()
+{
+    new("Apple", 1.99m, "A really delicious apple", DateTime.Now.AddDays(-1)),
+    new("Banana", 0.99m, "A yellow banana", DateTime.Now.AddDays(-5)),
+    new("Orange", 1.49m, "A juicy and big orange", DateTime.Now.AddDays(42)),
+};
+
+template = Template.Parse("""
+    <ul id="products">
+        {{- for product in Model.Products }}
+        <li>
+            <h2>{{ product.Name }}</h2>
+            Price: {{ product.Price | object.format "C" }}
+            {{ product.Description | string.truncate 15 }}
+            {{ product.Date | date.to_string '%d/%m/%Y %R' }}
+        </li>
+        {{- end }}
+    </ul>
+    """);
+
+context = CreateContext(new ProductsModel { Products = products });
+result = await template.RenderAsync(context);
+
+Console.WriteLine(result);
+Console.WriteLine();
+
+template = Template.Parse("""
+    <html>
+    <body>
+    <p>Hello {{ Model.Name }},</p>
+    <p>
+    Thank yout for registering. Please <a href="{{ Model.Url }}">click here</a> to activate your account
+    </p>
+    </body>
+    </html>
+    """);
+
+context = CreateContext(new ConfirmationEmailModel { Name = "Marco", Url = "https://www.example.com" });
+result = await template.RenderAsync(context);
+
+Console.WriteLine(result);
+Console.WriteLine();
+
+result = await OrderConfirmation.CreateOrderAsync();
+
+Console.WriteLine(result);
+
+static TemplateContext CreateContext(object model)
+{
+    var context = new TemplateContext { MemberRenamer = member => member.Name };
+
+    context.PushGlobal(new ScriptObject { { "Model", model } });
+    context.PushCulture(CultureInfo.CurrentCulture);
+
+    return context;
+}
+
+public record class Person(string FirstName, string LastName, string City);
+
+public record class Product(string Name, decimal Price, string Description, DateTime Date);
+
+public class PeopleModel
+{
+    public IEnumerable<Person> People { get; set; } = [];
+}
+
+public class ProductsModel
+{
+    public IEnumerable<Product> Products { get; set; } = [];
+}
+
+public class ConfirmationEmailModel
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Url { get; set; } = string.Empty;
+}
